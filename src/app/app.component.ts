c0b97364732bb0b8bbc24b37be6dfbf0d2820e31
maurofa <maurofabri@gmail.com>
2020-03-07 21:27:56 -0300

    Capítulo 4 do curso Ionic 3 parte 2.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
 import { ListaAgendamentoPage } from '../pages/lista-agendamento/lista-agendamento';
 import { LoginPage } from '../pages/login/login';
+import { PerfilPage } from '../pages/perfil/perfil';
+import { UsuariosServiceProvider } from '../providers/usuarios-service/usuarios-service';
 @Component({
   selector: 'myapp',
   templateUrl: 'app.html'
@@ -18,10 +19,20 @@ export class MyApp {
       titulo: 'Agendamentos',
       compomente: ListaAgendamentoPage.name,
       icone: 'calendar'
+    },
+    {
+      titulo: 'Perfil',
+      compomente: PerfilPage.name,
+      icone: 'person'
     }
   ];
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    private usuariosService: UsuariosServiceProvider
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -32,6 +43,10 @@ export class MyApp {
 
   irParaPagina(compomente) {
     this.nav.push(compomente);
+  }
+
+  get usuarioLogado() {
+    return this.usuariosService.obtemUsuarioLogado();
   }
 }
 
---
+++ src/pages/perfil/perfil.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { UsuariosServiceProvider } from '../../providers/usuarios-service/usuarios-service';
+
+@IonicPage()
+@Component({
+  selector: 'page-perfil',
+  templateUrl: 'perfil.html',
+})
+export class PerfilPage {
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private usuariosService: UsuariosServiceProvider
+  ) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad PerfilPage');
+  }
+
+  get usuarioLogado() {
+    return this.usuariosService.obtemUsuarioLogado();
+  }
+}
